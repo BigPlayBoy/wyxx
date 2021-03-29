4c00e76a97aa4d52ce9e188d03baecbee6392457
按时吃饭 <1151770629@qq.com>
2021-03-29 18:27:12 +0800

    使用fileloader处理js中的图片

--- W13/fileLoader/src/js/index.js
+++ W13/fileLoader/src/js/index.js
@@ -1,2 +1,9 @@
 import '../css/index.css';
+
+//js引入img图片
+import imgsrc from  '../images/banner01.png'
+console.log(imgsrc);
 console.log('index.js加载成功');
+const imgEl=document.createElement('img');
+imgEl.src=imgsrc;
+document.body.appendChild(imgEl);
